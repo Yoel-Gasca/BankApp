e1353fa71aa240cc3f55991c561883f42f2fb886
Yoel-Gasca <83617933+Yoel-Gasca@users.noreply.github.com>
2023-08-18 19:14:23 -0600

    Feching & using data

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-// implementar un mapa entre las rutas de URL
+//Variable para almacenar los datos
+let account = null;
+// Implementar un mapa entre las rutas de URL
 const routes = {
     '/login': { templateId: 'login', title: 'Login' },
-    '/dashboard': { templateId: 'dashboard', title: 'Dashboard' },
+    '/dashboard': { templateId: 'dashboard', title: 'Dashboard', init: 'updateDashboard' },
     '/404': { templateId: '404', title: 'Page not found' },
-    '/transaction': { templateId: 'transantion', title: 'Transaction'}
+    //'/transaction': { templateId: 'transantion', title: 'Transaction'}
 };
 
 // Recupere el elemento de plantilla en el DOM
@@ -28,6 +30,11 @@ function updateRoute(templateId) {
     if (templateId === 'dashboard') {
         console.log('Se muestra el panel');
     }
+
+    //
+    if (typeof route.init === 'function') {
+        route.init();
+      }
 }
 updateRoute('login');
 
@@ -37,19 +44,9 @@ function navigate(path) {
     updateRoute();
 }
 
-// Función de registro
-async function register() {
-    const registerForm = document.getElementById('registerForm');
-    const formData = new FormData(registerForm);
-    const data = Object.fromEntries(formData);
-    const jsonData = JSON.stringify(data);
-    const result = await createAccount(jsonData);
-
-    if (result.error) {
-        return console.log('An error occured:', result.error);
-    }
-    console.log('Account created!', result);
-}
+// ---------------------------------------------------------------------------
+// Login/register
+// ---------------------------------------------------------------------------
 
 // Funcion asincrona que envia los datos de usuario al servidor
 async function createAccount(account) {
@@ -64,6 +61,87 @@ async function createAccount(account) {
         return { error: error.message || 'Unknown error' };
     }
 }
+
+// Función del login
+async function login() {
+    const loginForm = document.getElementById('loginForm')
+    const user = loginForm.user.value;
+    const data = await getAccount(user);
+    if (data.error) {
+        return updateElement('loginError', data.error);
+    }
+    
+    account = data;
+    navigate('/dashboard');
+}
+
+// Función de registro
+async function register() {
+    const registerForm = document.getElementById('registerForm');
+    const formData = new FormData(registerForm);
+    const data = Object.fromEntries(formData);
+    const jsonData = JSON.stringify(data);
+    const result = await createAccount(jsonData);
+
+    if (result.error) {
+        return updateElement('registerError', result.error);
+    }
+
+    account = result;
+    navigate('/dashboard');
+}
+
+// Funcion que actualizará el contenido de texto del elemento DOM con el correspondiente id.
+function updateElement(id, text) {
+    const element = document.getElementById(id);
+    element.textContent = '';
+    element.append(textOrNode);
+}
+
+// Funcion para recuperar los datos de la cuenta
+async function getAccount(user) {
+    try {
+        const response = await fetch('//localhost:5000/api/accounts/' + encodeURIComponent(user));
+        return await response.json();
+    } catch (error) {
+        return { error: error.message || 'Unknown error' };
+    }
+}
+
+// ---------------------------------------------------------------------------
+// Dashboard
+// ---------------------------------------------------------------------------
+
+// Función para completar el marcador de posición:
+function updateDashboard() {
+    if (!account) {
+        return navigate('/login');
+    }
+  
+    updateElement('description', account.description);
+    updateElement('balance', account.balance.toFixed(2));
+    updateElement('currency', account.currency);
+
+    // Crea un nuevo fragmento DOM en el que podemos trabajar
+    const transactionsRows = document.createDocumentFragment();
+    for (const transaction of account.transactions) {
+        const transactionRow = createTransactionRow(transaction);
+        transactionsRows.appendChild(transactionRow);
+    }
+    updateElement('transactions', transactionsRows);
+}
+
+// Función que crea una nueva fila en la tabla y completa su contenido usando datos de transacciones.
+function createTransactionRow(transaction) {
+    const template = document.getElementById('transaction');
+    const transactionRow = template.content.cloneNode(true);
+    const tr = transactionRow.querySelector('tr');
+    tr.children[0].textContent = transaction.date;
+    tr.children[1].textContent = transaction.object;
+    tr.children[2].textContent = transaction.amount.toFixed(2);
+    return transactionRow;
+  }
+
 
 // Asegura de que la plantilla mostrada se actualice cuando cambie el historial del navegador
 window.onpopstate = () => updateRoute();
